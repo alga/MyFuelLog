de6df44ef5a77180cbaa916a21b778c639d9aaf3
Albertas Agejevas <alga@pov.lt>
2011-05-08 22:38:20 +0300

    Cleaned up the launcher icon, made icons for one tab.

--- gen/lt/pov/FuelLog/R.java
+++ gen/lt/pov/FuelLog/R.java
@@ -12,6 +12,8 @@ public final class R {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
+        public static final int tab_fill_deselected=0x7f020001;
+        public static final int tab_fill_selected=0x7f020002;
     }
     public static final class id {
         public static final int buttons=0x7f05000f;
